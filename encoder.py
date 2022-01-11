f3fb9e2c3435556e39845acf55add95c1e69c27c
Zxmon <68187602+Zxmon-code@users.noreply.github.com>
2022-01-11 10:07:58 +0100

    try

---
+++ encoder.py
@@ -0,0 +1,15 @@
+import time
+
+#The characters that can be encoded
+abc = ["a", "b", "c", "d"]
+
+#the list of characters how they get encoded
+cba = ["d", "c", "b", "a"]
+
+word = input(str("Which word would you like to encode: "))
+
+word_length = len(word)
+
+list_word = list.word
+
+for i in word
